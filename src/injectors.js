b4f5a9f01b63ebac25f6aa0737c9125379741bec
Josh Habdas <jhabdas@gmail.com>
2017-05-29 17:06:46 +0800

    docs(LICENSE): add contact method

--- src/injectors.js
+++ src/injectors.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2017, Josh Habdas
+ * Copyright (c) 2017, Josh Habdas <josh@habd.as>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
