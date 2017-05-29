4cb527107da36f4aa378484f341e85831a2aa729
Josh Habdas <jhabdas@gmail.com>
2017-05-29 15:01:28 +0800

    fix($module): check if blob type includes text/css
    whatwg suggests blob.type may include mime type properties
    
    closes #20, closes #21

--- rollup.config.js
+++ rollup.config.js
@@ -12,7 +12,7 @@ const config = {
   sourceMap: false,
   plugins: [
     license({
-      banner: `Fetch Inject v<%= pkg.version %>\nCopyright (c) <%= moment().format('YYYY') %> Josh Habdas\n@license ISC`
+      banner: `/*! Fetch Inject v<%= pkg.version %> | (c) <%= moment().format('YYYY') %> Josh Habdas | @license ISC */`
     })
   ]
 }
