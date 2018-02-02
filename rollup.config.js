fc1f018ca7c1ed0db836f887f9ee1d74849e785f
David Díaz <diazglezdavid@gmail.com>
2018-02-02 10:21:37 +0100

    feat(Rollup): transpile ES6 code through babel
    Using `babel-core`, `rollup-plugin-babel`, and `babel-preset-es2015-rollup` as dev dependencies.
    
    Fix #36, Fix #33

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
-import uglify from 'rollup-plugin-uglify'
+import babel from 'rollup-plugin-babel'
 import license from 'rollup-plugin-license'
+import uglify from 'rollup-plugin-uglify'
 import { minify } from 'uglify-es'
 
 const defaultConfig = {
@@ -28,6 +29,7 @@ const minifiedConfigs = activeConfigs.reduce(
   (minifiedConfigs, activeConfig) => minifiedConfigs.concat(
     Object.assign({}, activeConfig, {
       plugins: [
+        babel({ presets: ['es2015-rollup'], exclude: 'node_modules/**' }),
         uglify({}, minify),
         license({ banner: `/*! Fetch Inject v<%= pkg.version %> | © <%= moment().format('YYYY') %>, 13AMDq9isKtQTxMQG4w7Yo7cEhqKAqQ4Lz | @license BTC */` }),
         ...activeConfig.plugins
